65aceda509622d2feca2ec3ae301077e0c489447
Sabrina HR <86672447+sabrina7777@users.noreply.github.com>
2023-11-28 17:46:57 -0500

    initalcommit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SabrinaRoleApp.Models;
+
+namespace SabrinaRoleApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Job> Jobs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Job>().HasMany(j => j.NextJobs).WithMany();
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SabrinaRoleApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+
+        // To grab job associated with user
+        public Job Job { get; set; }
+    }
+}
